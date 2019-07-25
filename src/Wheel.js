d384211ac78028ccaecffc59de6705a71f40d59d
chrisjamesr <chrisjames.r@gmail.com>
2019-07-24 21:47:48 -0400

    add class name to parent element

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import wheel from './zodiac_wheel_modified.svg';
+import WheelDisplay from './WheelDisplay';
 
 function Wheel ({currentPosition}){ 
     return(
-        <section className="wheel animated" >
-              <img src={wheel} className="App-logo" alt="logo"
-                  style={{
-                    transform: `rotate(${currentPosition}deg)`
-                  }}
-              />
+        <section className="wheel animated" 
+                 style={
+                    {transform: `rotate(${currentPosition}deg)`}
+                  }>
+              <img src={wheel} className="App-logo" alt="logo"/>
+              
         </section>          
     );
 }
