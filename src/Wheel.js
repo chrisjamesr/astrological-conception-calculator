bc0db280ff93913789c78874846d56149072ed3f
chris <chrisjamesr@gmail.com>
2020-09-09 22:48:24 -0400

    add inline styling for image height/width

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import wheel from './zodiac_wheel_modified.svg';
-import WheelDisplay from './WheelDisplay';
+// import wheel from './zodiac_wheel_modified.svg';
+import wheel from './Zodiac_wheel.svg';
+// import WheelDisplay from './WheelDisplay
 
-function Wheel ({currentPosition}){ 
-    return(
-        <section className="wheel animated" 
-                 style={
-                    {transform: `rotate(${currentPosition}deg)`}
-                  }>
-              <img src={wheel} className="App-logo" alt="logo"/>
-              
-        </section>          
+function Wheel({ currentPosition }) {
+    return (
+        <section >
+            <img src={wheel}
+                className="wheel animated"
+                alt="zodiac wheel"
+                style={
+                    {
+                        transform: `rotate(${currentPosition}deg)`,
+                        height: "50%",
+                        width: "50%"
+                    }
+                }
+            />
+
+        </section>
     );
 }
 
